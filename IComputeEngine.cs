100350ca2e08f864ba51ad6a8285681334f708c3
Lelak271 <LOUis.perier@hotmail.com>
2026-04-19 22:04:31 +0200

    Ajoutez des fichiers projet.

---
+++ IComputeEngine.cs
@@ -0,0 +1,17 @@
+﻿using Toltech.ComputeEngine.Contracts;
+
+namespace Toltech.ComputeEngine.Contracts
+{
+    /// <summary>
+    /// Interface for the compute engine, responsible for performing calculations based on the provided requirements and returning the results in a concurrent dictionary format.
+    /// </summary>
+    public interface IComputeEngine
+    {
+        Task<ComputeResult> ComputeAsync(ComputeRequest request);
+
+        Task<bool> IsPartIsostaticAsync(List<ComputeModelData> modelData, ComputePart part);
+
+        Task<bool> IsModelIsostaticAsync(List<ComputeModelData> modelData, int partId1, int partId2, int idFixPart = 0);
+
+    }
+}
