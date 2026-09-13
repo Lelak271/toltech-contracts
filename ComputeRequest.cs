06c68747c5ec4d63b899921c9895bbfba73ea8c6
Lelak271 <LOUis.perier@hotmail.com>
2026-09-14 00:39:09 +0200

    Simplification du contract avec la suppression du legacy PrimaryResult devenu caduque => revoir le naming de la page request car confusion avec la class qui porte ce nom dedans,

--- ComputeRequest.cs
+++ ComputeRequest.cs
@@ -35,19 +35,10 @@ namespace Toltech.Solver.Contracts
         /// </summary>
         public bool IsSuccess { get; init; }
 
-        public ConcurrentDictionary<int, SolveResults> ResultsNew { get; init; }
-            = new();
         /// <summary>
-        /// Résultats bruts du calcul (structure actuelle conservée).
+        /// Stores the results of problem solving operations, indexed by an integer key of requirements.
         /// </summary>
-        public ConcurrentDictionary<int, List<PrimaryResults>> DetailedResults { get; init; }
-            = new();
-
-        /// <summary>
-        /// refacto 
-        /// </summary>
-        public ConcurrentDictionary<int, List<PrimaryResults>> Results { get; init; }
-            = new();
+        public ConcurrentDictionary<int, SolveResults> RawResults { get; init; } = new();
 
         /// <summary>
         /// Message d'erreur éventuel.
@@ -55,17 +46,9 @@ namespace Toltech.Solver.Contracts
         public string? ErrorMessage { get; init; }
     }
 
-
-    // TO DO voir comment cette classe passe la frontiere dll de IcomputeEngine
-    public sealed class PrimaryResults
-    {
-        public int IdData { get; set; }
-        public double InflX { get; set; }
-        public double InflY { get; set; }
-        public double InflZ { get; set; }
-
-    }
-
+    /// <summary>
+    /// 
+    /// </summary>
     public sealed class DecompositionResults
     {
         public int IdData { get; set; }
@@ -76,22 +59,20 @@ namespace Toltech.Solver.Contracts
 
     }
 
+    /// <summary>
+    /// Represents the results of a solving operation.
+    /// </summary>
     public sealed class SolveResults
     {
-        public List<PrimaryResults> Summary { get; init; } = new();
-
         public List<DecompositionResults> Details { get; init; } = new();
     }
 
-    public enum UnknownType
-    {
-        RotA,
-        RotB,
-        RotC,
-        Tu,
-        Tv,
-        Tw
-    }
+
+    #region Computes Datas
+
+    /// <summary>
+    /// DTO : Représente une liaison utilisée par le moteur de calcul Toltech.
+    /// </summary>
     public sealed class ComputeLinkage
     {
         public int Id { get; init; }
@@ -123,76 +104,26 @@ namespace Toltech.Solver.Contracts
         #endregion
 
 
-
         public string? Name { get; init; }
 
         public LinkageType Linkage { get; init; }
 
-
         public ToleranceTriplet N { get; init; } = new();
-
         public ToleranceTriplet T1 { get; init; } = new();
-
         public ToleranceTriplet T2 { get; init; } = new();
         public ToleranceTriplet Rn { get; init; } = new();
-
         public ToleranceTriplet RT1 { get; init; } = new();
         public ToleranceTriplet RT2 { get; init; } = new();
     }
 
-
-
-    public class ToleranceTriplet
-    {
-        public ToleranceDefinition Origin { get; set; } = new();
-
-        public ToleranceDefinition Intermediate { get; set; } = new();
-
-        public ToleranceDefinition Extremity { get; set; } = new();
-    }
-
-    public sealed class ToleranceDefinition
-    {
-        public double Value { get; set; }
-
-        public string Description { get; set; }
-
-        public string Name { get; set; }
-
-        public bool UseDatabase { get; set; }
-
-        public int ToleranceId { get; set; }
-    }
-
     /// <summary>
-    /// Types de liaison mécaniques possibles
+    /// DTO : Représente une exigence utilisée par le moteur de calcul Toltech.
     /// </summary>
-    public enum LinkageType
-    {
-        PointContact = 0,       // Liaison ponctuelle
-        LinearContact = 1,      // Liaison linéaire rectiligne
-        AnnularContact = 2,     // Liaison linéaire annulaire
-        PlanarContact = 3,      // Appui plan
-
-        RevoluteContact = 4,    // Liaison pivot
-        PrismaticContact = 5,   // Liaison glissière
-        CylindricalContact = 6, // Liaison pivot glissant
-
-        //HelicalContact = 7,   // Liaison hélicoïdale
-
-        SphericalContact = 8,   // Liaison rotule
-
-        //PinSlotContact = 9,   // Rotule à doigt
-
-        FixedContact = 10,       // Liaison encastrement
-        Requirement = 11       // Liaison encastrement
-    }
-
     public sealed class ComputeRequirement
     {
-        public int Id_req { get; init; }
+        public int Id { get; init; }
 
-        public string? NameReq { get; init; }
+        public string? Name { get; init; }
 
         public double CoordX { get; init; }
         public double CoordY { get; init; }
@@ -224,8 +155,7 @@ namespace Toltech.Solver.Contracts
     }
 
     /// <summary>
-    /// Représente une pièce utilisée par le moteur de calcul Toltech.
-    /// DTO pur, sans dépendance UI ou base de données.
+    /// DTO : Représente une pièce utilisée par le moteur de calcul Toltech.
     /// </summary>
     public sealed class ComputePart
     {
@@ -243,6 +173,81 @@ namespace Toltech.Solver.Contracts
 
         public bool IsActive { get; init; }
     }
+
+    #region Helper for tolerances objects
+   
+    /// <summary>
+    /// DTO : Représente les 3 tolérances d'un contact utilisée par le moteur de calcul Toltech.
+    /// </summary>
+    public class ToleranceTriplet
+    {
+        public ToleranceDefinition Origin { get; set; } = new();
+
+        public ToleranceDefinition Intermediate { get; set; } = new();
+
+        public ToleranceDefinition Extremity { get; set; } = new();
+    }
+
+    /// <summary>
+    /// DTO : Représente une tolérance utilisée par le moteur de calcul Toltech.
+    /// </summary>
+    public sealed class ToleranceDefinition
+    {
+        public double Value { get; set; }
+
+        public string Description { get; set; }
+
+        public string Name { get; set; }
+
+        public bool UseDatabase { get; set; }
+
+        public int ToleranceId { get; set; }
+    }
+    #endregion
+
+    #endregion
+
+    #region General properties
+
+    /// <summary>
+    ///  
+    /// </summary>
+    public enum UnknownType
+    {
+        RotA,
+        RotB,
+        RotC,
+        Tu,
+        Tv,
+        Tw
+    }
+
+    /// <summary>
+    /// Types de liaison mécaniques possibles
+    /// </summary>
+    public enum LinkageType
+    {
+        PointContact = 0,       // Liaison ponctuelle
+        LinearContact = 1,      // Liaison linéaire rectiligne
+        AnnularContact = 2,     // Liaison linéaire annulaire
+        PlanarContact = 3,      // Appui plan
+
+        RevoluteContact = 4,    // Liaison pivot
+        PrismaticContact = 5,   // Liaison glissière
+        CylindricalContact = 6, // Liaison pivot glissant
+
+        //HelicalContact = 7,   // Liaison hélicoïdale
+
+        SphericalContact = 8,   // Liaison rotule
+
+        //PinSlotContact = 9,   // Rotule à doigt
+
+        FixedContact = 10,       // Liaison encastrement
+        Requirement = 11       // Liaison encastrement
+    }
+   
+    #endregion
+
 
 
 }
